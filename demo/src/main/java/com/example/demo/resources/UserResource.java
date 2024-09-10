64847d49a2c9b5723b2bf5b59a5d29555a50ce87
Pedro Reimberg <pedro.reimberg.oliveira@gmail.com>
2024-09-10 17:03:01 -0300

    Criação do projeto, conectando aplicação ao MongoDB, instanciando base de dados e implementando padrão DTO

---
+++ demo/src/main/java/com/example/demo/resources/UserResource.java
@@ -0,0 +1,30 @@
+package com.example.demo.resources;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.User;
+import com.example.demo.dto.UserDTO;
+import com.example.demo.services.UserService;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<UserDTO>> findAll(){
+        List<User> list = service.findAll();
+        List<UserDTO> dto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(dto);
+    }
+}
